745288e3954f3110f53d1bc33df238f1dc22fe99
Humza Sajid <pak786hamzasajid66@Gmail.com>
2021-11-01 21:40:21 +0500

    created a dynamic pop over menu while typing on search product , included filter option as well

--- src/components/UI/Transfers.js
+++ src/components/UI/Transfers.js
@@ -12,14 +12,36 @@ import {
   PopoverContent,
   PopoverBody,
   PopoverTrigger ,
-  InputGroup 
+  InputGroup,
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  AlertDialogCloseButton ,
+  useDisclosure,
+  PopoverArrow,
+  
+  
   
 } from '@chakra-ui/react';
 
 import { SearchIcon, CalendarIcon, ChevronDownIcon } from '@chakra-ui/icons';
 
 function Transfers() {
+
+
   const [startDate, setStartDate] = useState(new Date());
+  
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+
+  function search()
+  {
+    
+
+  }
 
   return (
     <>
@@ -117,9 +139,153 @@ function Transfers() {
                 pointerEvents="none"
                 children={<SearchIcon color="gray.600" />}
               />
-              <Input type="tel" placeholder="Search products" />
+              <Input type="tel" placeholder="Search products"  onKeyPress={onOpen} />
+              <AlertDialog
+        motionPreset="slideInBottom"
+        leastDestructiveRef={cancelRef}
+        onClose={onClose}
+        isOpen={isOpen}
+        isCentered 
+        size="3xl"
+      >
+        <AlertDialogOverlay />
+
+        <AlertDialogContent>
+          <AlertDialogHeader >
+              <button className=" w-9 h-10 mb-4 ml-3 inline">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width={12}
+              height={12}
+              fill="currentColor"
+              className="bi bi-arrow-left w-6 h-6 text-gray-600 mx-auto"
+              viewBox="0 0 16 16"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"
+              />
+            </svg>
+          </button>
+               <h3 className="inline ml-5 -mt-2">All Products</h3>
+
+</AlertDialogHeader>
+<Divider/>
+          <AlertDialogCloseButton />
+          <AlertDialogBody>
+              <div className="flex flex-row">
+          <Input type="tel" placeholder="Search products"/>
+
+          <Popover>
+  <PopoverTrigger>
+  <Button variant="unstyled">
+      <button className=" p-1 ml-3 mr-3 border border-gray-300 rounded text-base font-medium w-32"> 
+      <svg xmlns="http://www.w3.org/2000/svg" className="text-gray-400 h-6 w-6 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 4a1 1 0 011-1h16a1 1 0 011 1v2.586a1 1 0 01-.293.707l-6.414 6.414a1 1 0 00-.293.707V17l-4 4v-6.586a1 1 0 00-.293-.707L3.293 7.293A1 1 0 013 6.586V4z" />
+</svg>
+      Filter
+      
+
+      
+      </button>
+
+    
+    </Button>
+
+  </PopoverTrigger>
+  <PopoverContent>
+    <PopoverArrow />
+    <PopoverBody>
+
+        <ul>
+<li className="mt-2">
+    <input type="radio" ></input>
+    <text className="ml-3">Barcode</text>
+</li>
+
+<li className="mt-2">
+    <input type="radio"></input>
+    <text className="ml-3">  SKU</text>
+  
+</li>
+<li className="mt-2">
+    <input type="radio"></input>
+    <text className="ml-3"> Variant ID</text>
+   
+</li>
+<li className="mt-2">
+    <input type="radio"></input>
+    <text className="ml-3">Variant title</text>
+    
+</li>
+
+            </ul>
+    </PopoverBody>
+  </PopoverContent>
+</Popover>
+      
+</div>
+<Divider className="mt-3"/>
+
+<div className="flex flex-row justify-between mt-4 mb-4">
+
+    <h2 className="font-medium">PRODUCTS</h2>
+    <h2 className="font-medium">TOTAL AVAILABLE
+</h2>
+</div>
+
+<Divider/>
+
+<div className="flex flex-row flex-wrap justify-between w-full h-20  items-center">
+
+<input
+      type="checkbox"
+       className="h-5 w-5 rounded border-2 border-gray-400"
+ ></input>
+<svg xmlns="http://www.w3.org/2000/svg" class="text-gray-600 h-8 w-24" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+</svg>
+
+<h2>NAME</h2>
+
+<button className="border border-blue-400 rounded-3xl bg-blue-300 w-20">Status</button>
+
+<h2 className="w-20">0</h2>
+</div>
+<Divider/>
+          </AlertDialogBody>
+          <AlertDialogFooter>
+              
+
+<div className="flex   w-full justify-start items-center content-center">
+
+<div className="flex-grow-3 w-full">
+
+    <button className=" p-3 bg-gray-100 text-gray-400 border border-gray-300 rounded text-base font-medium ">0 variant selected</button>
+    </div>
+    
+    <div>
+    <button ref={cancelRef} onClick={onClose} className="p-3 border border-gray-300 rounded text-base font-medium w-32">Cancel</button>
+    </div>
+    <div>
+    <button className="p-3 border text-gray-400 border border-gray-300 rounded text-base font-medium w-32">Add Transfer</button>
+    </div>
+    
+    
+</div>
+            {/* <Button ref={cancelRef} onClick={onClose}>
+              No
+            </Button>
+            <Button colorScheme="red" ml={3}>
+              Yes
+            </Button> */}
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+             
             </InputGroup>
             <Button
+           
               colorScheme="whiteAlpha"
               color="black"
               className="ml-2 border border-gray-300 rounded"
